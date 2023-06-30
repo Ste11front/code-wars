4cd8528fcee11e767c71a736ff53d88edef8cb56
Ste11front <Ste11front@users.noreply.github.com>
2023-06-30 15:02:29 +0200

    ex

--- exercise.js
+++ exercise.js
@@ -184,17 +184,119 @@ console.log(accum('abc'))
 //'A-' + 'B' + 'b' + '-' => 'A-Bb-'
 //'A-Bb-' + 'C' + 'cc' + '' => 'A-Bb-Ccc'
 
-console.log(3 === 3);
+// function reverseNumber(n){
+//     const nString = n + '';
+//     //console.log('nString', nString);
+//     // const nStringArray = [...nString];
+//     const nStringArray = nString.split('');
+//     //console.log("🚀 ~ file: exercise.js:191 ~ reverseNumber ~ nStringArray:", nStringArray);
+//     const reversedNStringArray = nStringArray.reverse();
+//     //console.log("🚀 ~ file: exercise.js:193 ~ reverseNumber ~ reversedNStringArray:", reversedNStringArray)
+//     const reversedNArray = reversedNStringArray.map((e) => parseInt(e));
+//     //console.log("🚀 ~ file: exercise.js:195 ~ reverseNumber ~ reversedNArray:", reversedNArray)
+//     return reversedNArray;
 
-let pippo = 5;
-let pluto = 5;
-console.log(pippo === pluto);
+// }
 
-let qui = 'paperino';
-let quo = 'paperino';
-console.log(qui === quo);
+// console.log(reverseNumber(9234))
 
-const student1 = {name: 'manuela', yob: 1988}
-const student2 = {name: 'manuela', yob: 1988}
 
-console.log(student1 === student2);
+
+// console.log(3 === 3);
+
+// let pippo = 5;
+// let pluto = 5;
+
+// console.log(pippo === pluto);
+
+// let qui = 'paperino';
+// let quo = 'paperino';
+
+// console.log(qui === quo);
+
+// const student1 = {name: 'manuela', yob: 1988};
+// const student2 = {name: 'luca', yob: 1999};
+// const student3 = {name: 'manuela', yob: 1988};
+
+// console.log(student1 === student2);
+// console.log(student1 === student3);
+
+// const student4 = student1;
+
+// student1.name = 'isabella'
+
+// console.log(student1);
+// console.log(student4);
+
+
+// const manuela = {
+//                     name: 'manuela',
+//                     yob: 1988,
+//                     dog:{
+//                         name:'nala'
+//                     }
+//                 }
+
+// Object.freeze(manuela);
+
+// manuela.yob = 2000;
+// manuela.dog.name = 'pluto';
+
+// console.log(manuela);
+
+
+
+// const luca = {name: 'luca', yob: 2005, hobby: 'gaming'};
+
+// console.log(luca);
+
+
+// luca.hobby = 'coding';
+
+// console.log(luca);
+
+
+// const array = [manuela, luca];
+
+
+// console.log(JSON.stringify(array));
+
+// manuela.dog.name = 'clarabella';
+
+// console.log(array);
+
+
+
+const number1 = 5;
+
+let damiano = {name: 'damiano', yob: 1993};
+
+
+function multiplyBy3(n) {
+    n = n * 3;
+    return n;
+}
+
+function multiplyYobBy3(person){
+    const result = person.yob * 3;
+    return result;
+}
+
+
+console.log(multiplyBy3(number1));
+console.log(number1);
+
+console.log(multiplyYobBy3(damiano));
+console.log(damiano);
+
+
+function capitalizePersonName(person){
+    // const newPerson = {name: person.name.toUpperCase(), yob: person.yob};
+    const newPerson = {...person}
+    newPerson.name = person.name.toUpperCase();
+    return newPerson;
+}
+
+damiano = capitalizePersonName(damiano);
+
+console.log(damiano)
